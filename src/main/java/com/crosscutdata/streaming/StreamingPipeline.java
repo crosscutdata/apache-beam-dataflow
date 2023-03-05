f12d185d4b111ee129058a4265c1297e5b497e7c
Md Sakibul Hasan <sakib.iut@gmail.com>
2023-03-05 13:50:43 -0500

    initial commit

---
+++ src/main/java/com/crosscutdata/streaming/StreamingPipeline.java
@@ -0,0 +1,32 @@
+package com.crosscutdata.streaming;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
+
+
+public class StreamingPipeline {
+
+	public static void main(String[] args) {
+
+		Options options = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
+		Pipeline p = Pipeline.create(options);
+
+		// This example reads from a public dataset containing the text of King Lear.
+		PCollection<String> input = p.apply("Read", TextIO.read().from("gs://apache-beam-samples/shakespeare/kinglear.txt"));
+		PDone done = input.apply("Write", TextIO.write().to("gs://bucket-wissen/dataflow/output-2.txt"));
+
+		PipelineResult result = p.run();
+		try {
+			result.getState();
+			result.waitUntilFinish();
+		} catch (UnsupportedOperationException e) {
+			// do nothing
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
